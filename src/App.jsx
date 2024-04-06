21c0a829325270f86c4ef4d7751f277e70959b93
VikasJassu <vksjassu@gmail.com>
2024-04-07 00:08:57 +0530

    modifications

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,16 @@ import "./App.css";
 import Card from "./components/Card";
 import { useEffect, useState } from "react";
 import ImageSlider from "./components/ImageSlider";
-import { setShowImage } from "./redux/imagesSlice";
 
 function App() {
   const [images, setImages] = useState();
-  const dispatch = useDispatch();
   const image = useSelector((state) => state.imageReducer.image);
   const { index, showImage } = useSelector((state) => state.imageReducer);
   useEffect(() => setImages(image));
   console.log("index", index);
 
   return (
-    <div className="relative">
+    <div className="container mx-auto relative">
       <div
         className={`flex justify-center items-center p-3 ${
           showImage ? "blur-sm" : ""
@@ -22,11 +20,12 @@ function App() {
       >
         <Card />
       </div>
-      <div className="absolute top-16 left-44">
-        {showImage && <div>
-          <div onClick={()=> dispatch(setShowImage(false))} className="text-xl text-black bg-slate-300 w-9 h-9 rounded-full aspect-square p-3 flex items-center justify-center absolute z-50 top-1 left-1 cursor-pointer hover:bg-slate-400">X</div>
-          <ImageSlider images={images} currentIndex={index} />
-        </div>}
+      <div className="absolute top-16 left-52 mx-auto">
+        {showImage && (
+          <div>
+            <ImageSlider images={images} currentIndex={index} />
+          </div>
+        )}
       </div>
     </div>
   );
--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -3,15 +3,19 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
+import { useDispatch} from "react-redux";
+import { setShowImage } from "../redux/imagesSlice";
 
 const ImageSlider = ({ images, currentIndex }) => {
   console.log(currentIndex);
+  const dispatch = useDispatch();
   return (
-    <div className="">
+    <div className="fixed">
       <Swiper modules={[Navigation]} navigation={true} slidesPerView={1} initialSlide={currentIndex} className="w-[70vw]">
         {images.map((image, index) => (
           <SwiperSlide key={index} className="">
             <img src={image.imageUrl} alt="image" className="object-cover w-full h-[80vh] rounded-md" />
+            <div onClick={()=> dispatch(setShowImage(false))} className="text-xl text-black bg-slate-300 w-9 h-9 rounded-full aspect-square p-3 flex items-center justify-center absolute z-50 top-1 left-1 cursor-pointer hover:bg-slate-400">X</div>
           </SwiperSlide>
         ))}
       </Swiper>
